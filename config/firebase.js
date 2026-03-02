6852fee2a1b1e61138043b0e7e09c16139bb528d
Hernán Arévalo <hernanarevalo16@gmail.com>
2026-03-02 12:26:22 -0300

    chore: update dependencies in package.json
    - Removed "@vercel/analytics" version "1.6.1" from dependencies.
    - Re-added "@vercel/analytics" version "1.6.1" to maintain consistency.
    - Added "firebase" dependency with version "^12.10.0".

---
+++ config/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore/lite'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDerTvq8CLDwol0OdqUrFsq_XdM4o4L73o",
+  authDomain: "app-jardin-buron.firebaseapp.com",
+  projectId: "app-jardin-buron",
+  storageBucket: "app-jardin-buron.firebasestorage.app",
+  messagingSenderId: "934272365646",
+  appId: "1:934272365646:web:3df56f47987826a9cbff1f",
+  measurementId: "G-WXYFWKCX51"
+};
+
+// Initialize Firebase
+const FirebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(FirebaseApp);
+
+export const FirebaseAuth = getAuth( FirebaseApp );
+export const FirebaseDB   = getFirestore( FirebaseApp );
+
