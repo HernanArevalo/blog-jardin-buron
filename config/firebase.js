ea2cffe5647a787cc754fa052cf14566e1401c8e
Hernán Arévalo <hernanarevalo16@gmail.com>
2026-03-18 13:00:42 -0300

    update firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -1,33 +1,33 @@
-import { initializeApp } from "firebase/app"
-import { getAnalytics, isSupported } from "firebase/analytics"
+import { initializeApp, getApps, getApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
- 
+import { getAnalytics, isSupported } from "firebase/analytics"
 
- const firebaseConfig = {
-   apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-   authDomain: "app-jardin-buron.firebaseapp.com",
-   projectId: "app-jardin-buron",
-   storageBucket: "app-jardin-buron.firebasestorage.app",
-   messagingSenderId: "934272365646",
-   appId: "1:934272365646:web:3df56f47987826a9cbff1f",
-
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "app-jardin-buron.firebaseapp.com",
+  projectId: "app-jardin-buron",
+  storageBucket: "app-jardin-buron.firebasestorage.app",
+  messagingSenderId: "934272365646",
+  appId: "1:934272365646:web:3df56f47987826a9cbff1f",
   measurementId: "G-WXYFWKCX51",
 }
- 
- const FirebaseApp = initializeApp(firebaseConfig)
- 
- export const FirebaseAuth = getAuth(FirebaseApp)
- export const FirebaseDB = getFirestore(FirebaseApp)
-export const FirebaseStorage = getStorage(FirebaseApp)
- 
- let analytics
- 
- if (typeof window !== "undefined") {
-   isSupported().then((yes) => {
-     if (yes) {
-       analytics = getAnalytics(FirebaseApp)
-     }
-   })
+
+// evita doble init
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
+
+export const FirebaseAuth = getAuth(app)
+export const FirebaseDB = getFirestore(app)
+export const FirebaseStorage = getStorage(app)
+
+// analytics SOLO en cliente
+let analytics = null
+
+if (typeof window !== "undefined") {
+  isSupported().then((yes) => {
+    if (yes) analytics = getAnalytics(app)
+  })
 }
+
+export { analytics }
